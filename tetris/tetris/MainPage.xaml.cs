a5cc25ee4eb9d9fb99c45f940d23149968eb6c19
kimminkuk <rlaal567@gmail.com>
2021-02-02 21:49:44 +0900

    Comment Delete

--- tetris/tetris/MainPage.xaml.cs
+++ tetris/tetris/MainPage.xaml.cs
@@ -11,57 +11,16 @@ namespace tetris
 {
     public partial class MainPage : ContentPage
     {
-        public static MainXamarinViewModels MX = new MainXamarinViewModels();
         public MainPage()
         {
-            
-            
-            //Device.StartTimer(new TimeSpan(0, 0, 1), () =>
-            //{
-            //    // do something every 60 seconds
-            //    Device.BeginInvokeOnMainThread(() =>
-            //    {
-            //        // interact with UI elements
-            //        BindingContext = new MainXamarinViewModels();
-            //    });
-            //    return true; // runs again, or false to stop
-            //});
-            //((MainXamarinViewModels)BindingContext).EnterGame();
-
             InitializeComponent();
             BindingContext = new MainXamarinViewModels();
-            
-            
-            //this.BindingContext = new MainXamarinViewModels();
-            //this.BindingContext = MX;
-
-          //  this.BindingContext = MX;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
             MainXamarinViewModels MV = (MainXamarinViewModels)BindingContext;
             MV.EnterGame();
-            //MX.EnterGame();
-            //MX.EnterGame();
-            //this.BindingContext = MainPage.BindingContextProperty;
-            //BindingContext = new MainXamarinViewModels();
-            //((MainXamarinViewModels)BindingContext).EnterGame();
-            //Device.StartTimer(new TimeSpan(0, 0, 1), () =>
-            //{
-            //    // do something every 60 seconds
-            //    Device.BeginInvokeOnMainThread(() =>
-            //    {
-            //        // interact with UI elements
-            //        MainXamarinViewModels mm = new MainXamarinViewModels();
-            //        this.BindingContext = mm;
-            //    });
-            //    return true; // runs again, or false to stop
-            //});
-
-            //MainXamarinViewModels mainXamarinViewModels = new MainXamarinViewModels();
-            //this.BindingContext = mainXamarinViewModels;
         }
 
         private void Button_Clicked_LEFT(object sender, EventArgs e)
